565eae2cd3237864bdc170f0663e21fde01ca622
Alexys Santiago <suxport@hotmail.com>
2026-06-13 10:23:25 -0300

    test: add coveragerc configuration and update workflow to run all tests

--- tests/test_app_bad.py
+++ tests/test_app_bad.py
@@ -12,14 +12,8 @@ def test_adicionar_produto_quantidade_invalida():
         cart.adicionar_produto("Teclado Mecânico", 350.00, 0)
 
 def test_forcar_falha_calculo_total():
-    """
-    Este teste simula um erro lógico forçando uma asserção falsa.
-    Como a SmartShopCloud calcula o total corretamente, o pytest
-    irá falhar neste assert, derrubando o pipeline.
-    """
     cart = SmartShopCart()
-    # Adiciona R$ 150.00 em produtos
     cart.adicionar_produto("Monitor Gamer", 150.00, 1)
     
-    # FORÇA A FALHA: Compara 150.00 com um valor errado (999.99)
-    assert cart.calcular_total() == 999.99
+    # Mudando para 150.0 para o teste passar com sucesso
+    assert cart.calcular_total() == 150.00
